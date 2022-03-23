eefaa35868170757705ccb93ad59cc9b932d2d88
9names <60134748+9names@users.noreply.github.com>
2022-03-24 00:17:13 +1100

    Switch to using Rust-based svdtools. Remove + add src to git after each run to catch add/rm files

--- update.sh
+++ update.sh
@@ -7,10 +7,11 @@ cargo install --version 0.8.0 form
 cargo install --version 0.2.1 svdtools
 rustup component add rustfmt
 
+git rm -r -f src
 rm -rf src
 mkdir src
 
-svd patch svd/bl702.yaml
+svdtools patch svd/bl702.yaml
 
 svd2rust -i svd/bl702.svd.patched --target riscv
 
@@ -18,3 +19,4 @@ form -i lib.rs -o src
 rm lib.rs
 
 cargo fmt
+git add src
